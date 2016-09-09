fc5680736bd87669dc3b83ce3cef440c41728ed2
wchang28 <wchang28@hotmail.com>
2016-09-09 17:35:55 -0400

    added debug code

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 module.exports.get = function(options) {
 	return (function(req, res, next) {
 		if (req.headers['x-prettyprint']) {
-			console.log('***********************************');
-			console.log(res.json);
-			console.log('***********************************');
 			res.json = function(obj) {
-				var s = JSON.stringify(obj, null, 2);
-				res.header('content-type', 'application/json; charset=utf-8');
-				res.end(s);
+				var body = JSON.stringify(obj, null, 2);
+				if (!res.get('Content-Type'))
+					res.set('Content-Type', 'application/json');
+				res.send(body);
 			};
 		}
 		next();
