90df430810db83dae8109ced6c97e8e946de028b
wchang28 <wchang28@hotmail.com>
2016-09-09 17:14:40 -0400

    added debug code

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ module.exports.get = function(options) {
 	return (function(req, res, next) {
 		if (req.headers['x-prettyprint']) {
 			res.json = function(obj) {
-				res.end(JSON.stringify(obj, null, 2));
+				var s = JSON.stringify(obj, null, 2);
+				console.log('=================================================');
+				console.log(typeof res.app.get('etag'));
+				console.log('');
+				console.log(res.app.get('etag'));
+				console.log('=================================================');
+				console.log('');
+				res.header('content-type', 'application/json; charset=utf-8');
+				res.end(s);
 			};
 		}
 		next();
